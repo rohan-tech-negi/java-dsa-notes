3f0c4e03aff62b2fdf4dacf4f81aa63c0fb0629a
rohan-tech-negi <rohanwork953@gmail.com>
2026-02-25 13:00:35 +0530

    insertion sort

--- sorting algoritm/insertionSort.java
+++ sorting algoritm/insertionSort.java
@@ -1,5 +1,24 @@
 
 
 public class insertionSort {
-    
+    public static void printarrays(int arr[]) {
+        for(int i = 0; i<arr.length ; i++){
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {3, 4,8 ,9,10};
+        for (int i=0; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j >=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        printarrays(arr);
+    }
 }
